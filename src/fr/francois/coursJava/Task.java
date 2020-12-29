2c283eec8fe37f2368902ae03c38d1384862f6b3
Francois-legrand <yyperite@hotmail.fr>
2020-12-29 13:03:28 +0100

    add comment

--- src/fr/francois/coursJava/Task.java
+++ src/fr/francois/coursJava/Task.java
@@ -1,10 +1,12 @@
 package fr.francois.coursJava;
 
+// class Task
 public class Task {
 	public String title;
 	public String description;
 	public boolean completed = false;
 	
+	// Task contructor
 	public Task(String desc, String titl) {
 		description = desc;
 		title = titl;
