cc29ec0920ed2ff8263a2fa0f8b6ba123d904f36
Développeur web <51367556+Francois-Legrand@users.noreply.github.com>
2021-01-17 21:17:09 +0100

    Mot clé Static

---
+++ src/fr/francois/coursJava/stati/People.java
@@ -0,0 +1,17 @@
+package fr.francois.coursJava.stati;
+
+public class People {
+	private String nom;
+	private int age;
+	protected static int nombreDePersonne;
+	
+	public People(String nom, int age) {
+		this.nom = nom;
+		this.age = age;
+		nombreDePersonne++;
+	}
+	
+	
+	
+	
+}
---
+++ src/fr/francois/coursJava/stati/TestPeople.java
@@ -0,0 +1,14 @@
+package fr.francois.coursJava.stati;
+
+public class TestPeople {
+
+	public static void main(String[] args) {
+		People franck = new People("Franck", 40);
+		People tony = new People("Tony", 46);
+		
+		System.out.println(People.nombreDePersonne);
+		
+
+	}
+
+}
